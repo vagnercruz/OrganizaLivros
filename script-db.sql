a76e4cea5b5293ede9ea7b377402427933cf2ab2
vagnercruz <vagnerdacruz48@gmail.com>
2025-05-17 21:01:56 -0300

    feat(database): alterando tabelas no script do banco, referente aos generos de livros

--- script-db.sql
+++ script-db.sql
@@ -9,6 +9,23 @@ CREATE TABLE livros (
     paginas INT,
     data_aquisicao DATE,
     preco DECIMAL(10,2),
-    capa TEXT
+    capa TEXT,
+    status_leitura VARCHAR(20),
+	data_inicio DATE,
+	data_fim DATE,
+	nota INTEGER,
+	anotacoes TEXT
+);
+
+
+CREATE TABLE generos (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) UNIQUE NOT NULL
+);
+
+CREATE TABLE livro_genero (
+    livro_id INT REFERENCES livros(id) ON DELETE CASCADE,
+    genero_id INT REFERENCES generos(id) ON DELETE CASCADE,
+    PRIMARY KEY (livro_id, genero_id)
 );
 
